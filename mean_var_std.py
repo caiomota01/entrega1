52bc7908ea4161e4cab054a33886df8eb7e4e1b2
Caio Mota <caio.mota@sou.inteli.edu.br>
2025-08-22 17:24:56 +0000

    Parte 1 da Entrega 1 - mean-variance-standard Deviation Calculator

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,43 @@
 import numpy as np
 
-def calculate(list):
+def calculate(numbers):
+    if len(numbers) != 9:
+        raise ValueError("A lista deve conter nove números")
+    
+    # Converte a lista em matriz 3x3
+    matrix = np.array(numbers).reshape(3, 3)
 
-
-
+    calculations = {
+        'mean': [
+            list(np.mean(matrix, axis=0)),
+            list(np.mean(matrix, axis=1)),
+            float(np.mean(matrix))
+        ],
+        'variance': [
+            list(np.var(matrix, axis=0)),
+            list(np.var(matrix, axis=1)),
+            float(np.var(matrix))
+        ],
+        'standard deviation': [
+            list(np.std(matrix, axis=0)),
+            list(np.std(matrix, axis=1)),
+            float(np.std(matrix))
+        ],
+        'max': [
+            list(np.max(matrix, axis=0)),
+            list(np.max(matrix, axis=1)),
+            int(np.max(matrix))
+        ],
+        'min': [
+            list(np.min(matrix, axis=0)),
+            list(np.min(matrix, axis=1)),
+            int(np.min(matrix))
+        ],
+        'sum': [
+            list(np.sum(matrix, axis=0)),
+            list(np.sum(matrix, axis=1)),
+            int(np.sum(matrix))
+        ]
+    }
 
     return calculations
